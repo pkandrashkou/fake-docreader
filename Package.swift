e8336ad49e0b39a1f2ad2a23383f5f2a8ba148ab
pkondrashkov <pkondrashkov@gmail.com>
2021-07-22 16:23:47 +0300

    +

--- Package.swift
+++ Package.swift
@@ -5,18 +5,16 @@ let package = Package(
     name: "FakeDocumentReader",
     products: [
         // .library(name: "FaceSDK", targets: ["FaceSDK-Target"])
-        .library(name: "FakeDocumentReader", targets: ["FakeDocumentReader", "RegulaCommon"])
+        .library(name: "FakeDocumentReader", targets: ["FakeDocumentReader"])
+    ],
+    dependencies: [
+        .package(name: "RegulaCommon", url: "https://github.com/pkondrashkov/common", .branch("master")),
     ],
     targets: [
         .binaryTarget(
             name: "FakeDocumentReader",
             url: "https://pods.regulaforensics.com/RegulaCommon/doc-reader/FakeDocumentReader.xcframework.zip",
             checksum: "a57b85077f1476db5ba44918923dca36945e7f54266ad38e1bf184b369f4370d"
-        ),
-        .binaryTarget(
-            name: "RegulaCommon",
-            url: "https://pods.regulaforensics.com/RegulaCommon/regula-common/RegulaCommon.xcframework.zip",
-            checksum: "9ef592c40d960420c0dba3c49e97f2f3071baaff60a1e00b2b9d99f6cf6d656a"
         ),
         // .target(
         //     name: "FaceSDK-Target",
